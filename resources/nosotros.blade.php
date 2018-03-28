4d232a6fa6d52282ac60ddc2bb6501ad681078ed
paoloxc <postmaster@constructivo.com>
2018-03-28 08:59:04 -0500

    se agregaron 2 vistas y se actualizo estilo

---
+++ resources/nosotros.blade.php
@@ -0,0 +1,225 @@
+@extends('layouts.front')
+@section('content')
+
+<div class="fila sectoradjust">
+		<div class="contenedor">
+			<div class="row">
+			<section class="col-xs-12 col-sm-12 col-md-9 rightline">
+
+				<section class="sectiontitulo">ACERCA DE NOSOTROS</section>
+
+				<div class="extrapost">
+					<div class="row">
+						<section class="col-xs-12 col-sm-12 col-md-12">
+							<div class="postdata">
+								Publicación de vanguardia dentro de la arquitectura peruana, que conjuga el carácter técnico informativo con el enfoque arquitectónico y el diseño interior que demanda el profesional de hoy.<br>
+								Selecciona en cada número una tipología de edificación: Casas Urbanas, Departamentos, Espacios Corporativos, Flats & Lofts, Tendencias Contemporáneas, Casas de Campo, Condominios, Casas de Playa, Diseño interior y otros.
+							</div>
+						</section>
+					</div>
+				</div>
+
+				<div class="extrapost">
+					<div class="row">
+						<div class="mostrador">
+							<div class="revista"><img src="img/campo.png" class="img-responsive" alt=""></div>
+							<div class="revista"> <img src="img/corporativos.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/departamentos.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/edificios.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/flats.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/oficina.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/playa.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/tendencias.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/interior.png" class="img-responsive"  alt=""> </div>
+							<div class="revista"> <img src="img/urbanas.png" class="img-responsive"  alt=""> </div>
+						</div>
+					</div>
+				</div>
+
+				<div class="extrapost">
+					<div class="row">
+						<div class="subtitulo"> Nuestra Misión </div>
+						<p>Brindar una selección cuidadosamente editada de la mejor arquitectura, diseño y proyectos de interiores con fotografías, planos y sus descripciones, tanto nacionales como internacionales.</p>
+					</div>
+				</div>
+
+				<div class="extrapost">
+					<div class="row">
+						<div class="subtitulo"> Grupo Objetivo </div>
+						<p>Arquitectos, decoradores y profesionales interesados en el tema a tratar.</p>
+					</div>
+				</div>
+
+
+				<!-- BANNER LATERAL -->
+				<div class="fila sectoradjust text-center">
+					<div class="bonus">
+						<a href="#" target="_blank"><img src="banner/suscription728x90.gif" alt=""></a>
+					</div>
+					<div class="bonusmobile">
+						<a href="#" target="_blank"><img src="banner/suscription363x90.gif" alt=""></a>
+					</div>
+				</div>
+				<!-- BANNER LATERAL -->
+
+			</section>
+			<!-- COLUMNA ASIDE -->
+			<section class="col-xs-12 col-sm-12 col-md-3 hidden-xs">
+
+				<div class="fila topadjust">
+					<section class="sectiontitulo">VIDEO DESTACADO</section>
+					<div class="video">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/9SQfKYZsIXA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						<h1> Expodeco 2018 - VI Salón Internacional del Diseño y Arquitectura. </h1>
+					</div>
+					<a href="#" class="enlace">Ver videos</a>
+				</div>
+
+				<div class="fila sectoradjust">
+					<section class="sectiontitulo">TENDENCIAS</section>									
+					
+					<div class="trending bottomline" style="display: flex;">
+						<section class="col-xs-3 col-sm-3 col-md-1 flexrank">1</section>
+						<section class="col-xs-9 col-sm-9 col-md-11 bannerfila">
+							<section class="titlerank">	<a href="#">Presentan en Chile "inmobiliaria popular" que arrendará viviendas a familias vulnerables</a> </section>
+							<section class="imagerank">
+								<img src="post/noticia.jpg" alt="">
+							</section>
+						</section>
+					</div>
+
+					<div class="trending bottomline" style="display: flex;">
+						<section class="col-xs-3 col-sm-3 col-md-1 flexrank">2</section>
+						<section class="col-xs-9 col-sm-9 col-md-11 bannerfila">
+							<section class="titlerank">	<a href="#">Presentan en Chile "inmobiliaria popular" que arrendará viviendas a familias vulnerables</a> </section>
+							<section class="imagerank">
+								<img src="post/noticia.jpg" alt="">
+							</section>
+						</section>
+					</div>
+
+					<div class="trending bottomline" style="display: flex;">
+						<section class="col-xs-3 col-sm-3 col-md-1 flexrank">3</section>
+						<section class="col-xs-9 col-sm-9 col-md-11 bannerfila">
+							<section class="titlerank">	<a href="#">Presentan en Chile "inmobiliaria popular" que arrendará viviendas a familias vulnerables</a> </section>
+							<section class="imagerank">
+								<img src="post/noticia.jpg" alt="">
+							</section>
+						</section>
+					</div>
+
+					<div class="trending bottomline" style="display: flex;">
+						<section class="col-xs-3 col-sm-3 col-md-1 flexrank">4</section>
+						<section class="col-xs-9 col-sm-9 col-md-11 bannerfila">
+							<section class="titlerank">	<a href="#">Presentan en Chile "inmobiliaria popular" que arrendará viviendas a familias vulnerables</a> </section>
+							<section class="imagerank">
+								<img src="post/noticia.jpg" alt="">
+							</section>
+						</section>
+					</div>
+
+					<div class="trending bottomline" style="display: flex;">
+						<section class="col-xs-3 col-sm-3 col-md-1 flexrank">5</section>
+						<section class="col-xs-9 col-sm-9 col-md-11 bannerfila">
+							<section class="titlerank">	<a href="#">Presentan en Chile "inmobiliaria popular" que arrendará viviendas a familias vulnerables</a> </section>
+							<section class="imagerank">
+								<img src="post/noticia.jpg" alt="">
+							</section>
+						</section>
+					</div>
+				</div>
+
+				<!-- BANNER LATERAL -->
+				<div class="fila sectoradjust">
+					<div class="lateralbanner">
+						<div class="sponsored">Publicidad</div>
+						<img src="banner/expodeco2018.gif" alt="">
+					</div>
+				</div>
+				<!-- BANNER LATERAL -->
+
+
+				<div class="fila sectoradjust">
+					<div class="suscription">
+						<section class="sectiontitulo">Suscríbete al Dossier de Arquitectura</section>
+						<div class="row">
+
+							<section class="col-xs-12 col-sm-8 col-md-8">								
+								<h4>ESPACIOS CORPORATIVOS</h4>
+								Presentamos las últimas novedades en Espacios Corporativos Ed. 35
+								<div class="fila topadjust">
+									<button type="button" class="btn btn-default">Comprar</button>
+									<button type="button" class="btn btn-success">Suscribirse</button>
+								</div>
+							</section>
+							<section class="col-xs-12 col-sm-4 col-md-4">								
+								<img src="edicion/portada35.png" class="img-responsive" alt="">
+							</section>
+							
+							
+						</div>
+					</div>
+				</div>
+
+				<div class="fila sectoradjust">
+					<section class="sectiontitulo bottomline">EXPLORAR</section>
+					<div class="temas adjuster">
+						<section><a href="#">Casas</a></section>
+						<section><a href="#">Departamentos</a></section>
+						<section><a href="#">Inmobiliarias</a></section>
+						<section><a href="#">Construcciones</a></section>
+						<section><a href="#">Casas de Campo</a></section>
+						<section><a href="#">Flats</a></section>
+						<section><a href="#">Lofts</a></section>
+						<section><a href="#">Casas</a></section>
+						<section><a href="#">Departamentos</a></section>
+						<section><a href="#">Inmobiliarias</a></section>
+						<section><a href="#">Construcciones</a></section>
+						<section><a href="#">Casas de Campo</a></section>
+						<section><a href="#">Flats</a></section>
+						<section><a href="#">Lofts</a></section>
+						<section><a href="#">Casas</a></section>
+						<section><a href="#">Departamentos</a></section>
+						<section><a href="#">Inmobiliarias</a></section>
+						<section><a href="#">Construcciones</a></section>
+						<section><a href="#">Casas de Campo</a></section>
+						<section><a href="#">Flats</a></section>
+						<section><a href="#">Lofts</a></section>
+						<section><a href="#">Casas</a></section>
+						<section><a href="#">Departamentos</a></section>
+						<section><a href="#">Inmobiliarias</a></section>
+						<section><a href="#">Construcciones</a></section>
+						<section><a href="#">Casas de Campo</a></section>
+						<section><a href="#">Flats</a></section>
+						<section><a href="#">Lofts</a></section>						
+					</div>
+				</div>
+
+				<!-- BANNER LATERAL -->
+				<div class="fila sectoradjust">
+					<div class="lateralbanner">
+						<div class="sponsored">Publicidad</div>
+						<img src="banner/expodeco2018.gif" alt="">
+					</div>
+				</div>
+				<!-- BANNER LATERAL -->
+
+				<!-- BANNER LATERAL -->
+				<div class="fila sectoradjust">
+					<div class="lateralbanner">
+						<div class="sponsored">Publicidad</div>
+						<img src="banner/expodeco2018.gif" alt="">
+					</div>
+				</div>
+				<!-- BANNER LATERAL -->
+
+
+
+			</section>
+			<!-- COLUMNA ASIDE -->
+		</div>
+		</div>
+	</div>
+	<!-- CONTENIDO -->
+
+@endsection
